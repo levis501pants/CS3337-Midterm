da16e9266e03740ebcc01e0832b2dd9d130d3614
Levi A. Onyiah <Levi@Levi-A-Onyiah.local>
2017-09-30 15:18:53 -0700

    I wish I get A+ in this course

---
+++ src/helloworld.java
@@ -0,0 +1,12 @@
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Name: Levi Onyiah");
+		System.out.println("Class: CS3337 Saturdays");
+
+	}
+
+}
